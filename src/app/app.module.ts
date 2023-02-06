99354cd3bb748896b888d015c76a4a483982a792
Antenor <nanaosei452@gmail.com>
2023-02-06 17:25:04 +0000

    created top-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
