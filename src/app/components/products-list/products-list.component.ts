8a6c23483dc765bbe7367a252754e2fab8173e82
Antenor <nanaosei452@gmail.com>
2023-02-07 08:55:59 +0000

    created product interface

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from 'src/app/interfaces/product';
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ProductsListComponent {
 
+  products:Product[] = [
+    {
+      id:1,
+      'name':'Phone XL',
+      'description':'A large phone with one of the best screens'
+    },
+    {
+      id:2,
+      'name':'Phone Mini',
+      'description':'A greatphone with one of the best cameras'
+    },
+    {
+      id:3,
+      'name':'Phone Standard',
+      'description':'A simple phone'
+    }
+  ];
 }
