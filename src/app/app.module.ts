b06665a6e729a2b73c8867d45bec7a367a1a01a6
Antenor <nanaosei452@gmail.com>
2023-02-07 09:24:39 +0000

    created product item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ButtonComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductComponent
 
   ],
   imports: [
